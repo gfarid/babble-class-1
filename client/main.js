39c1eb10594ed8827322e7230222be9a92ba7a81
krulik <iwillkissyou@gmail.com>
2016-11-17 22:12:48 +0200

    ajaxify forms

--- client/main.js
+++ client/main.js
@@ -2,26 +2,34 @@
 
 var form1 = document.querySelector('form');
 form1.addEventListener('submit', function (e) {
-
-  var submit = confirm('Are you sure?');
-  if (submit === false) {
-    e.preventDefault();
-  }
+  e.preventDefault();
+  sendRequest('GET', form1.getAttribute('action'), onResponse);
 });
 
 var form2 = document.querySelectorAll('form')[1];
 form2.addEventListener('submit', function (e) {
   e.preventDefault();
-  console.dir(form2);
-  var input = document.querySelector('#message-input');
-  console.log(input.value);
+  var input = document.querySelector('#echo-input');
+  sendRequest('POST', form2.getAttribute('action'), onResponse, 'echo=' + input.value);
 });
 
+var form3 = document.querySelectorAll('form')[2];
+form3.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var input = document.querySelector('#message-input');
+  var input1 = document.querySelector('#name-input');
+  sendRequest('POST', form3.getAttribute('action'), onResponse, 'message=' + input.value + '&username=' + input1.value);
+});
+
+ajaxify(form1, function (e) {
+  console.log('ajax response for form1');
+});
+
+function ajaxify (form, callback) {
+
+}
+
 // Ajax
-// sendRequest('GET', 'http://localhost:9001/status', onResponse);
-// sendRequest('POST', 'http://localhost:9001/echo', onResponse, 'message=hi');
-
-
 function sendRequest (method, url, callback, data) {
   var xhr = new XMLHttpRequest();
   xhr.addEventListener('load', callback);
